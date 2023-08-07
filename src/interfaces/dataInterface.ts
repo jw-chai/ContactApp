9b127cb7e18b81239f1bad8ef8ed756119b5ceda
jw-chai <chaichaidev@gmail.com>
2023-08-07 21:34:06 +0800

    build contact screen

---
+++ src/interfaces/dataInterface.ts
@@ -0,0 +1,7 @@
+export interface IData {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email?: string;
+  phone?: string;
+}
--- src/views/ContactScreen/ContactScreen.tsx
+++ src/views/ContactScreen/ContactScreen.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import {SafeAreaView, View, StyleSheet, Text} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import {Color} from '../../constants';
-import {Button, Header} from '../../components';
+import {Header} from '../../components';
+import {IData} from '../../interfaces/dataInterface';
+import data from '../../models/api/data.json';
 
 interface IContactScreenProps {
   navigation: any;
 }
 
 const ContactScreen: React.FC<IContactScreenProps> = ({navigation}) => {
+  const renderItems = ({item}: {item: IData}) => {
+    return (
+      <TouchableOpacity
+        style={styles.contactList}
+        onPress={() => {
+          navigation.navigate('EditContactScreen');
+        }}>
+        <View style={styles.avatar} />
+        <Text style={styles.paragraph} numberOfLines={2}>
+          {item?.firstName} {item?.lastName}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <>
       <SafeAreaView style={styles.safeAreaViewHeader} />
       <SafeAreaView style={styles.safeAreaView}>
         <Header title="Contact list" />
         <View style={styles.container}>
-          <Text>ContactScreen</Text>
-          <Button
-            title="try navigate"
-            onPress={() => navigation.navigate('EditContactScreen')}
+          <FlatList
+            data={data}
+            renderItem={renderItems}
+            keyExtractor={(item: IData) => item.id.toString()}
           />
         </View>
       </SafeAreaView>
@@ -37,6 +61,27 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+  },
+  contactList: {
+    flexDirection: 'row',
+    alignItems: 'center',
     padding: 18,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    flexShrink: 1,
+  },
+  avatar: {
+    borderRadius: 35,
+    height: 70,
+    width: 70,
+    marginRight: 16,
+    backgroundColor: '#eee',
+  },
+  paragraph: {
+    fontSize: 16,
+    letterSpacing: 0.5,
+    lineHeight: 24,
+    color: Color.text,
   },
 });
